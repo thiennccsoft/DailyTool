ede187c8f3cc6d4b577c9a61b92f5ee473c1e242
DoHue97 <dohue97yp1@gmai.com>
2018-06-30 12:43:34 +0700

    add auth,user controller

---
+++ DTValueObjects/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTValueObjects.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+    }
+}
---
+++ DailyTool/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DTBLL.Controllers;
+using DTValueObjects;
+using DTValueObjects.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyTool.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        UserDTcontroller controller = new UserDTcontroller();
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginViewModel model)
+        {
+            vUsers user = new vUsers
+            {
+                UserName = model.UserName,
+                PassWord = model.PassWord
+            };
+            var userCheck = controller.CheckLogin(user);
+            return new OkObjectResult(userCheck);
+        }
+
+    }
+}
